f8aa42b773ddfd07ed449b1547582ac3e39f9bf8
Esteban Gonzalez <esteban.gonzalez@mail.utoronto.ca>
2026-03-03 15:22:16 -0500

    Updated files to reflect package naming conventions

--- submissions/package_compiling/temp_package_comp.R
+++ submissions/package_compiling/temp_package_comp.R
@@ -2,10 +2,10 @@ usethis::create_package("/University/Year4/STA380/simSeriesACF/")
 
 usethis::use_testthat(3)
 
-usethis::use_vignette("my-vignette")
-
 devtools::document()
 
 devtools::build_vignettes()
+
+usethis::use_vignette("my-vignette")
 
 devtools::check()
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(simSeriesACF)
+library(TsimSeriesACF)
 
-test_check("simSeriesACF")
+test_check("TsimSeriesACF")
